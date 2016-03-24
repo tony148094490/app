8dcd814b8826159ff8f3141423ae5241f80884f7
Zhao <zhaowz@685b3582f7bc.ant.amazon.com>
2016-03-24 09:34:03 -0700

    got the substitution working

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@ import webapp2
 
 form="""
 <form action="/rot13" method="post">
-    <textarea name="text"  > this is called textarea </textarea>
+    <textarea name="text">%(inputString)s</textarea>
     <input type="submit">
 </form>
 """
@@ -12,6 +12,10 @@ class Rot13Handler(webapp2.RequestHandler):
     global alphabet
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+    def write_form(self, inputString=""):
+        self.response.write(form % {"inputString": inputString})
+
+
     def rotate(self, inputString):
         if inputString == '':
             return inputString
@@ -19,19 +23,21 @@ class Rot13Handler(webapp2.RequestHandler):
         inputString = inputString.lower()
         result = ''
         for character in inputString:
-            result = result + alphabet[(character + 13)%26]
+            currentPosition = ord('z') - ord(character)
+            newPosition = currentPosition + 13
+            result = result + alphabet[(newPosition)%26]
         return result    
 
     def get(self):
         self.response.headers['Content-Type'] = 'html'
-        self.response.write(form)
+        self.write_form("Yeah right")
 
     def post(self):
         inputString = self.request.get("text")
 
         punctuation_character = ['.', '!', '?']
 
-        list1 = []
+        list1 = ''
         tempString = ''
 
         for character in inputString:
@@ -39,17 +45,14 @@ class Rot13Handler(webapp2.RequestHandler):
             if character in alphabet:
                 tempString += character
             else:
-                list1.append(self.rotate(tempString))
-                list1.append(character)
+                list1 += (self.rotate(tempString))
+                list1 += character
                 tempString = ''
 
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(list1)
-
+        self.write_form(list1)
 
 
 
 app = webapp2.WSGIApplication([
     ('/rot13',Rot13Handler),
 ], debug=True)
-
