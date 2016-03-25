11f8c1a274e15ac2a654cd57397309bc4faed4bd
Zhao <zhaowz@685b3582f7bc.ant.amazon.com>
2016-03-25 09:56:31 -0700

    first working version ?

--- helloworld.py
+++ helloworld.py
@@ -9,28 +9,38 @@ form="""
 
 class Rot13Handler(webapp2.RequestHandler):
     
-    global alphabet
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    global alphabetLower
+    alphabetLower = 'abcdefghijklmnopqrstuvwxyz'
+    global alphabetUpper
+    alphabetUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
     def write_form(self, inputString=""):
         self.response.write(form % {"inputString": inputString})
-
 
     def rotate(self, inputString):
         if inputString == '':
             return inputString
 
-        inputString = inputString.lower()
         result = ''
+        
         for character in inputString:
-            currentPosition = ord('z') - ord(character)
-            newPosition = currentPosition + 13
-            result = result + alphabet[(newPosition)%26]
+            isUpperCase = (ord(character) - ord('a'))
+            if(isUpperCase < 0):
+                currentPosition = ord('Z') - ord(character)
+                newPosition = currentPosition + 13
+                result = result + alphabetUpper[(newPosition)%26]
+
+            else:
+                currentPosition = ord('z') - ord(character)
+                newPosition = currentPosition + 13
+                result = result + alphabetLower[(newPosition)%26]
+
+
         return result    
 
     def get(self):
         self.response.headers['Content-Type'] = 'html'
-        self.write_form("Yeah right")
+        self.write_form("This form will rotate your character to the right 13 times!")
 
     def post(self):
         inputString = self.request.get("text")
@@ -41,17 +51,20 @@ class Rot13Handler(webapp2.RequestHandler):
         tempString = ''
 
         for character in inputString:
-            character = character.lower()
-            if character in alphabet:
+            if character in alphabetLower or character in alphabetUpper:
                 tempString += character
             else:
                 list1 += (self.rotate(tempString))
                 list1 += character
                 tempString = ''
 
+        if tempString != '':
+            if len(tempString) > 1 or (len(tempString) == 1 and (tempString in alphabetLower or tempString in alphabetUpper)):
+                list1 += (self.rotate(tempString))
+            else:
+                list1 += tempString
+
         self.write_form(list1)
-
-
 
 app = webapp2.WSGIApplication([
     ('/rot13',Rot13Handler),
